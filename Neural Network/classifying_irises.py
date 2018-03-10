3e7d4776795fab0bb1d02a692615a0cadcb3cc7d
Natalia <ndibbern@lion.lmu.edu>
2018-03-10 14:54:02 -0800

    train step all at once

--- Neural Network/classifying_irises.py
+++ Neural Network/classifying_irises.py
@@ -23,30 +23,29 @@ def load_iris(path):
 if __name__ == '__main__':
     values, output = load_iris('./iris.csv')
     size = 150
-    ratio = 0.9
+    ratio = 0.7
     train_size = int(size * ratio)
     test_size = size - train_size
 
-    train_x, train_y = values[:train_size, :], output[:train_size]
-    test_x, test_y = values[train_size:, :].reshape((test_size, 4)), output[train_size:].reshape((test_size, 3))
+    train_x = values[:train_size, :].reshape((train_size, 4))
+    train_y = output[:train_size].reshape((train_size, 3))
+    test_x = values[train_size:, :].reshape((test_size, 4))
+    test_y = output[train_size:].reshape((test_size, 3))
 
     x = tf.placeholder(tf.float32, shape=(None, 4))
     y = tf.placeholder(tf.float32, shape=(None, 3))
     W = tf.Variable(tf.random_uniform([4, 3]))
     b = tf.Variable(tf.random_uniform([1, 3]))
 
-    model = tf.sigmoid(tf.matmul(x, W) + b)
+    model = tf.nn.softmax(tf.matmul(x, W) + b)
     cost = -tf.reduce_sum(y * tf.log(model))
-    optimize = tf.train.GradientDescentOptimizer(learning_rate=0.99).minimize(cost)
+    optimize = tf.train.GradientDescentOptimizer(learning_rate=0.5).minimize(cost)
 
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
 
-        for i in range(train_size):
-            x_i = train_x[i].reshape((1, 4))
-            y_i = train_y[i].reshape((1, 3))
-            sess.run(optimize, feed_dict={x: x_i, y: y_i})
+        sess.run(optimize, feed_dict={x: train_x, y: train_y})
 
         print('AVG Loss: ', sess.run(cost, feed_dict={x: test_x, y: test_y})/test_size)
 
